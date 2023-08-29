4e7f96a8861134a2e380280198e3221dd0c2d828
Tung Nguyen <shodydosh@gmail.com>
2023-08-29 13:27:28 +0700

    feat: Adding charts

--- components/Admin/Homepage.tsx
+++ components/Admin/Homepage.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Charts from './Homepage/Charts'
+import Statistic from './Homepage/Statistic'
 
 const Homepage : React.FC = () => {
   return (
-    <div>Homepage</div>
+    <>
+      <Statistic/>
+      <Charts/>
+    </>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { AppProps } from "next/app";
 
 import 'animate.css';
 import "../styles/index.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
